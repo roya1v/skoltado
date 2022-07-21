36cfc2d8bbd97b929272533796f4c800d7a3347f
Mike JR <mikedalvik@ya.ru>
2022-07-21 09:26:46 +0200

    Initial Commit

---
+++ Skoltado/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Skoltado
+//
+//  Created by Mike Shevelinsky on 21.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
